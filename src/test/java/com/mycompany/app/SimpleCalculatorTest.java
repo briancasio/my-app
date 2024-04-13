6733b2c417f40bcfc602e2c7c1b7f32d55528953
briancasio <113497684+briancasio@users.noreply.github.com>
2024-04-13 00:23:45 -0600

    Second commit

---
+++ src/test/java/com/mycompany/app/SimpleCalculatorTest.java
@@ -0,0 +1,21 @@
+package com.mycompany.app;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimpleCalculatorTest {
+
+    @Test
+    public void twoPlusTwoShouldEqualFour() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        assertEquals(4, calculator.add(2, 2));
+    }
+
+    @Test
+        public void threePlusSevenShouldEqualTen() {
+        SimpleCalculator calculator = new SimpleCalculator();
+        assertEquals(10, calculator.add(3, 7));
+    }
+
+}
